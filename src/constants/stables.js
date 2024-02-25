41920773a43d9c1b69c92eb83fb524cf575766fc
RealHariKrishna <harikrishnamachavaram90@gmail.com>
2024-02-25 18:51:03 +0530

    updated dependencies

--- src/constants/stables.js
+++ src/constants/stables.js
@@ -1,4 +1,5 @@
-const UPLOAD_FOLDER_BASE_URL = "http://localhost:5555/uploads/";
+const UPLOAD_FOLDER_BASE_URL =
+  "https://blogapp-backend-1.onrender.com/uploads/";
 
 const stables = { UPLOAD_FOLDER_BASE_URL };
 
